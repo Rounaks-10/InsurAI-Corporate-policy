83155362a6097d59205a34b1515f9d700a8181f4
Rounaks-10 <rounakssultane@gmail.com>
2026-01-21 14:40:45 +0530

    Added .env file for backend api globally

--- insurai-frontend/src/api.js
+++ insurai-frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "https://insurai-corporate-policy-1.onrender.com";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const API = axios.create({
   baseURL: API_BASE_URL,
